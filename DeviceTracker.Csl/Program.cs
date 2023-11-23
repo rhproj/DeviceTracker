1a3d8faeb90f3fd78de362c44ae39d378cbf534d
Rh Proj <r.proj@hotmail.com>
2023-11-23 20:30:34 +0300

    Add project files.

---
+++ DeviceTracker.Csl/Program.cs
@@ -0,0 +1,20 @@
+﻿
+using System.Xml;
+using System.Xml.Linq;
+
+string path = @"C:\Proj\FlashDrive.xml";
+
+//XDocument doc = XDocument.Load();
+
+XmlDocument xml = new();
+xml.Load(path);
+
+var xList = xml.SelectNodes("/propertiesmap/key/tSTRING[@name='DcTrustedDeviceComment']");
+
+foreach (XmlNode x in xList)
+{
+    Console.WriteLine(x.InnerText);
+}
+
+// See https://aka.ms/new-console-template for more information
+//Console.WriteLine(doc);
